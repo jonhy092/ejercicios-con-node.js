fd05da393197a2de2977092daa3245496d17f8d8
jonhy092 <jonhy_092@hotmail.com>
2025-10-07 07:23:29 -0300

    manejo de archivos con FS

---
+++ modulos/fs.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+function leer(ruta, cb){
+    fs.readFile(ruta,(err, data)=>{
+        cb(data.toString());
+
+
+    })
+}
+
+function escribir(ruta, contenido, cb){
+    fs.writeFile(ruta,contenido,function(err){
+        if(err){
+            console.error('no he podido escribirlo', err);
+        } else {
+            console.log('se ha escrito muy bien');
+        }
+    })
+}
+
+function borrar(ruta,cb){
+    fs.unlink(ruta,cb);
+}
+
+
+//escribir(__dirname + '/archivo1.txt, Soy un archivo nuevo', console.log);
+//leer (__dirname + '/archivo.txt', console.log); 
+//borrar(__dirname + '/archivo1.txt', console.log);
+
+
